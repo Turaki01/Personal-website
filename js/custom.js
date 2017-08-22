407653de274cda0fd6238018130d72ac243d5142
Turaki ezekiel <ezekiel_zakari@yahoo.com>
2017-08-22 22:10:43 +0100

    first commit

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+//spinner/preloader
+$(window).load(function(){
+  $('.preloader').fadeOut(1000); // set duration in brackets
+});
+
+//smoothscroll
+$(function() {
+  $('#home a').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 49
+    }, 1000);
+    event.preventDefault();
+  });
+});
+
+
+// WOW Animation js
+new WOW({ mobile: false }).init();
+
+
+//parallax-section
+function initParallax() {
+  $('#home').parallax("50%", 50);
+  $('#service').parallax("50%", 40);
+  $('#about').parallax("50%", 20);
+  $('#work').parallax("50%", 30);
+  $('#contact').parallax("50%", 10);
+  }
+initParallax();
